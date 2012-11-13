dc384bbcc3226c03fe93f0c1ae0bd785f436b441
colley <colley@git.lsstcorp.org>
2012-11-13 18:18:02 +0000

    create

---
+++ libauxteles/kurucz.py
@@ -0,0 +1,18 @@
+'''
+Created on 13 nov. 2012
+
+@author: colley
+'''
+
+class Kurucz93(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self._version = 93
+        
