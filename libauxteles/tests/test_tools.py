b2fc029e2451233d21ee2b3229c9e1433048d390
colley <colley@git.lsstcorp.org>
2012-11-13 16:37:00 +0000

    refactoring

---
+++ libauxteles/tests/test_tools.py
@@ -0,0 +1,28 @@
+'''
+Created on 6 nov. 2012
+
+@author: colley
+'''
+
+
+import unittest
+from tools import *
+
+
+
+class Test(unittest.TestCase):
+
+
+    def test_01(self):
+        ret = readTextFileColumn('file_tools_01.txt',';')       
+        print ret
+        
+        
+    def test_02(self):
+        ret = readTextFileColumn('/home/colley/temp/lsst/modtran/TemplateT04.01_1.txt')        
+        print ret
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
