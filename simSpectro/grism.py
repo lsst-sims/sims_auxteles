8ce8f8c1908919fba0b0131e540e9e0881d41545
colley <colley@apc.univ-paris7.fr>
2013-04-05 17:46:32 +0200

    clean import

--- simSpectro/grism.py
+++ simSpectro/grism.py
@@ -12,7 +12,7 @@
 # and class definitions
 import math
 from constants import *
-from scipy import *
+import numpy as np
 
 
 
@@ -21,8 +21,8 @@ class grism:
     def __init__(self):
         # define some useful quantities
         # Sellmeier coefficients
-        self.B = array([1.03961212, 2.31792344e-1, 1.01046945])
-        self.C = array([6.00069867e3, 2.00179144e4, 1.03560653e8])
+        self.B = np.array([1.03961212, 2.31792344e-1, 1.01046945])
+        self.C = np.array([6.00069867e3, 2.00179144e4, 1.03560653e8])
         # grating characteristic
         self.a = 1./100.e3 # m
         # prism angle
@@ -30,13 +30,13 @@ class grism:
 
     def getN(self, l):
         # return the indice of the prism as a function of the wavelength
-        return sqrt(1. + sum( [ self.B[i]*l*l/(l*l-self.C[i]) for i in range(3) ] ))
+        return np.sqrt(1. + sum( [ self.B[i]*l*l/(l*l-self.C[i]) for i in range(3) ] ))
 
     def getT(self, l):
         # return the transmission of the prism
         # normal indicence for the input beam
-        kair = 2.*pi/l
-        kverre = 2.*pi*self.getN(l)/l
+        kair = 2.*np.pi/l
+        kverre = 2.*np.pi*self.getN(l)/l
         t = 2.*kair/(kair+kverre)
         # output beam
         theta1 = self.gratingAngle(l) + self.prismAngle
